9d9c411f4272ee889c966a48b3a3dbb924b1d701
Tom Richardson <tom@ajrmail.co.uk>
2013-06-19 22:44:37 +0100

    Changes to app and utils

--- PiMote/PiMote Android App/src/com/uom/pimote/Communicator.java
+++ PiMote/PiMote Android App/src/com/uom/pimote/Communicator.java
@@ -6,114 +6,122 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Communicator extends Activity {
 
-	TCPClient tcp;
+    private static final int NORMAL_CONTROL = 0;
+    private static final int JOYSTICK_CONTROL = 1;
+    private static int controlType = -1;
+    TCPClient tcp;
+    String ip;
+    int port;
+    LinearLayout layout;
+    AsyncTask<String, String, TCPClient> task;
+    RegularButtonManager regular = null;
+    ControllerManager cm = null;
+    boolean setup = false;
 
-	String ip;
-	int port;
-	LinearLayout layout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	private static final int NORMAL_CONTROL = 0;
-	private static final int JOYSTICK_CONTROL = 1;
+        try {
+            Bundle b = getIntent().getExtras();
+            port = b.getInt("port");
+            ip = b.getString("ip");
+        } catch (Exception e) {
+            endActivity("Bad Arguments");
+        }
+        layout = (LinearLayout) findViewById(R.id.mainlayout);
+        task = new connectTask().execute("");
+    }
 
-	AsyncTask<String, String, TCPClient> task;
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 
-	RegularButtonManager regular = null;
-	ControllerManager cm = null;
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("pi", "Ending");
+        tcp.stopClient();
+        task.cancel(true);
+    }
 
-	boolean setup = false;
-	private static int controlType = -1;
+    public void endActivity(String msg) {
+        Intent i = new Intent(this, Main.class);
+        Bundle b = new Bundle();
+        b.putString("pr", msg);
+        i.putExtras(b);
+        startActivity(i);
+        finish();
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onBackPressed() {
+        endActivity("");
+    }
 
-		try {
-			Bundle b = getIntent().getExtras();
-			port = b.getInt("port");
-			ip = b.getString("ip");
-		} catch (Exception e) {
-			endActivity("Bad Arguments");
-		}
-		layout = (LinearLayout) findViewById(R.id.mainlayout);
-		task = new connectTask().execute("");
-	}
+    public class connectTask extends AsyncTask<String, String, TCPClient> {
 
-	public class connectTask extends AsyncTask<String, String, TCPClient> {
+        @Override
+        protected TCPClient doInBackground(String... message) {
 
-		@Override
-		protected TCPClient doInBackground(String... message) {
+            // we create a TCPClient object and
+            tcp = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                // here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    // this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
 
-			// we create a TCPClient object and
-			tcp = new TCPClient(new TCPClient.OnMessageReceived() {
-				@Override
-				// here the messageReceived method is implemented
-				public void messageReceived(String message) {
-					// this method calls the onProgressUpdate
-					publishProgress(message);
-				}
+                public void failActivity(String msg) {
+                    endActivity(msg);
+                }
+            }, ip, port);
+            tcp.run();
 
-				public void failActivity(String msg) {
-					endActivity(msg);
-				}
-			}, ip, port);
-			tcp.run();
+            return null;
+        }
 
-			return null;
-		}
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            final String[] info = values[0].split(",");
+            // Log.d("pi", info[0] + ", " + info[1]);
 
-		@Override
-		protected void onProgressUpdate(String... values) {
-			super.onProgressUpdate(values);
-			final String[] info = values[0].split(",");
-			// Log.d("pi", info[0] + ", " + info[1]);
+            if (!setup) {
+                Log.e("TCPClient", "Setting up");
+                controlType = Integer.parseInt(info[0]);
+                if (controlType == JOYSTICK_CONTROL)
+                    cm = new ControllerManager(Communicator.this, tcp,
+                            Integer.parseInt(info[1]));
+                else
+                    regular = new RegularButtonManager(Communicator.this, tcp,
+                            layout);
+                setup = true;
+            } else {
+                int type = Integer.parseInt(info[0]);
+                String[] setup = new String[info.length-1];
+                for(int i = 1; i < info.length; i++){
+                    setup[i-1] = info[i];
+                }
+                if (type== 0) {
+                    if (controlType == NORMAL_CONTROL) {
 
-			if (!setup) {
-				Log.e("TCPClient", "Setting up");
-				controlType = Integer.parseInt(info[0]);
-				if (controlType == JOYSTICK_CONTROL)
-					cm = new ControllerManager(Communicator.this, tcp,
-							Integer.parseInt(info[1]));
-				else
-					regular = new RegularButtonManager(Communicator.this, tcp,
-							layout);
-				setup = true;
-			} else {
-				if (controlType == NORMAL_CONTROL) {
-					regular.addButtons(info);
-				}
+                        regular.addButtons(setup);
+                    }
+                }else if(type== 1){ //request to change text on a textview
+                    Log.e("TCPClient", "Text is to be changed to: " + setup[1]);
+                    TextView output = regular.getTextView(Integer.parseInt(setup[0]));
+                    output.setText(setup[1]);
+                }
 
-			}
-		}
-	}
-
-	protected void onPause() {
-		super.onPause();
-		finish();
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Log.d("pi", "Ending");
-		tcp.stopClient();
-		task.cancel(true);
-	}
-
-	public void endActivity(String msg) {
-		Intent i = new Intent(this, Main.class);
-		Bundle b = new Bundle();
-		b.putString("pr", msg);
-		i.putExtras(b);
-		startActivity(i);
-		finish();
-	}
-
-	@Override
-	public void onBackPressed() {
-		endActivity("");
-	}
+            }
+        }
+    }
 }
--- PiMote/PiMote Android App/src/com/uom/pimote/RegularButtonManager.java
+++ PiMote/PiMote Android App/src/com/uom/pimote/RegularButtonManager.java
@@ -9,80 +9,119 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TableRow;
-import android.widget.ToggleButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.util.Log;
+import java.util.ArrayList;
 
 public class RegularButtonManager {
 
+
+    //TextView[] outputs;
     TCPClient tcp;
     Context c;
     LinearLayout layout;
+    ArrayList<TextView> outputs;
 
     public RegularButtonManager(Context c, TCPClient tcp, LinearLayout layout) {
         this.c = c;
         this.tcp = tcp;
         this.layout = layout;
         ((Communicator) c).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        outputs = new ArrayList<TextView>();
     }
 
     public void addButtons(final String[] setup) {
-        if (Integer.parseInt(setup[0]) == 1) { // REGULAR BUTTON
-            Button button = new Button(c);
-            button.setText(setup[2]);
-            button.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    if (tcp != null)
-                        tcp.sendMessage(setup[1] + "," + " ");
-                }
-            });
-            layout.addView(button);
-        } else if (Integer.parseInt(setup[0]) == 2) { // BUTTON WITH TEXT
-            LinearLayout textButtonLayout = new LinearLayout(c);
-            LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
-                    LayoutParams.WRAP_CONTENT);
-            textButtonLayout.setLayoutParams(params);
-            final EditText addText = new EditText(c);
-            addText.setHint(setup[2]);
-            LayoutParams params2 = new TableRow.LayoutParams(0,
-                    LayoutParams.WRAP_CONTENT, 1f);
-            addText.setLayoutParams(params2);
-            Button button = new Button(c);
-            button.setText("Send");
-            button.setGravity(3);
-            button.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    if (tcp != null)
-                        tcp.sendMessage(setup[1] + ","
-                                + addText.getText().toString());
-                    addText.setText("");
-                }
-            });
-            textButtonLayout.addView(addText);
-            textButtonLayout.addView(button);
-            layout.addView(textButtonLayout);
-        } else if (Integer.parseInt(setup[0]) == 3) {
-            LinearLayout textButtonLayout = new LinearLayout(c);
-            LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
-                    LayoutParams.WRAP_CONTENT);
-            textButtonLayout.setLayoutParams(params);
-            TextView text = new TextView(c);
-            text.setText(setup[2]);
-            LayoutParams params2 = new TableRow.LayoutParams(0,
-                    LayoutParams.WRAP_CONTENT, 1f);
-            text.setLayoutParams(params2);
-            ToggleButton button = new ToggleButton(c);
-            boolean checked = Integer.parseInt(setup[3]) == 1;
-            button.setChecked(checked);
-            button.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int tf = ((ToggleButton) view).isChecked() ? 1 : 0;
-                    tcp.sendMessage(setup[1] + "," + tf);
-                }
-            });
-            textButtonLayout.addView(text);
-            textButtonLayout.addView(button);
-            layout.addView(textButtonLayout);
+        switch (Integer.parseInt(setup[0])) {
+            case 1:
+                addNewButton(setup);
+                Log.e("TCPClient", "Setup");
+                break;
+            case 2:
+                addNewTextInput(setup);
+                break;
+            case 3:
+                addNewToggle(setup);
+                break;
+            case 4: addNewTextView(setup);
+                break;
         }
+    }
+
+    public void addNewButton(final String[] setup) {
+        Button button = new Button(c);
+        button.setText(setup[2]);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if (tcp != null)
+                    tcp.sendMessage(setup[1] + "," + " ");
+            }
+        });
+        layout.addView(button);
+    }
+
+    public void addNewTextInput(final String[] setup) {
+        LinearLayout textButtonLayout = new LinearLayout(c);
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+        textButtonLayout.setLayoutParams(params);
+        final EditText addText = new EditText(c);
+        addText.setHint(setup[2]);
+        LayoutParams params2 = new TableRow.LayoutParams(0,
+                LayoutParams.WRAP_CONTENT, 1f);
+        addText.setLayoutParams(params2);
+        Button button = new Button(c);
+        button.setText("Send");
+        button.setGravity(3);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if (tcp != null)
+                    tcp.sendMessage(setup[1] + ","
+                            + addText.getText().toString());
+                addText.setText("");
+            }
+        });
+        textButtonLayout.addView(addText);
+        textButtonLayout.addView(button);
+        layout.addView(textButtonLayout);
+    }
+
+    public void addNewToggle(final String[] setup) {
+        LinearLayout textButtonLayout = new LinearLayout(c);
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+        textButtonLayout.setLayoutParams(params);
+        TextView text = new TextView(c);
+        text.setText(setup[2]);
+        LayoutParams params2 = new TableRow.LayoutParams(0,
+                LayoutParams.WRAP_CONTENT, 1f);
+        text.setLayoutParams(params2);
+        ToggleButton button = new ToggleButton(c);
+        boolean checked = Integer.parseInt(setup[3]) == 1;
+        button.setChecked(checked);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int tf = ((ToggleButton) view).isChecked() ? 1 : 0;
+                tcp.sendMessage(setup[1] + "," + tf);
+            }
+        });
+        textButtonLayout.addView(text);
+        textButtonLayout.addView(button);
+        layout.addView(textButtonLayout);
+    }
+
+    public void addNewTextView(final String[] setup) {
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+        TextView text = new TextView(c);
+        text.setLayoutParams(params);
+        text.setText(setup[2]);
+        layout.addView(text);
+        outputs.add(text);
+    }
+
+    public TextView getTextView(int id){
+        return outputs.get(id);
     }
 }
