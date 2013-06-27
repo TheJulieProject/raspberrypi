4c8ef33bdaa19ae7ae83a3e295ac78cc96343a1c
dimanui <abelgad2@cs.manchester.ac.uk>
2013-06-27 16:26:57 +0100

    Updated comments

--- projects/image_processing/background_removal.py
+++ projects/image_processing/background_removal.py
@@ -2,11 +2,11 @@
 from imgproc import *
 import time
 
-# open the webcam
+# Open the webcam
 cam= Camera(20, 15)
 
-# open a view, setting the view to the size of the captured image
-view = Viewer(cam.width, cam.height, "Blob finding")
+# View for the final image
+view = Viewer(cam.width, cam.height, "Background removed")
 
 while True:
 
@@ -14,15 +14,15 @@ while True:
 	my_image = cam.grabImage()	
 
 	# iterate over ever pixel in the image by iterating 
-	# over each row and each column
+	# over each row and column
 	for x in range(0, my_image.width):
   	  for y in range(0, my_image.height):
     		# get the value of the current pixel
     		red, green, blue = my_image[x, y]
 
-   		# check if the blue intensity is greater than the green
+   		# check if the intensities are near to white colour
    		if green > 200 and red > 200 and blue > 200:      		 
-      			# this pixel is predominantly blue
+      			# this pixel is predominantly white
       			# let's set it to black
       			my_image[x, y] = 0, 0, 0	
 
