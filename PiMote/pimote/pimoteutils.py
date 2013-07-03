99ea3daae52ececd3d51debccf1aa0b8e6f3c133
Tom Richardson <tom@ajrmail.co.uk>
2013-07-03 14:42:27 +0100

    things and stuff

--- PiMote/pimote/pimoteutils.py
+++ PiMote/pimote/pimoteutils.py
@@ -27,199 +27,199 @@ def createKey():
   file.write(privateKey)
 
 class Socket():
-	"""
-	Mutable wrapper class for sockets.
-	"""
+  """
+  Mutable wrapper class for sockets.
+  """
 
-	def __init__(self, socket):
-		# Store internal socket pointer
-		self._socket = socket
-	
-	def send(self, msg):
-		# Ensure a single new-line after the message
-		self._socket.send("%s\n" % msg.strip())
-		
-	def close(self):
-		self._socket.close()
-		
+  def __init__(self, socket):
+    # Store internal socket pointer
+    self._socket = socket
+  
+  def send(self, msg):
+    # Ensure a single new-line after the message
+    self._socket.send("%s\n" % msg.strip())
+    
+  def close(self):
+    self._socket.close()
+    
 
 class Receiver():
-	"""
-	A class for receiving newline delimited text commands on a socket.
-	"""
+  """
+  A class for receiving newline delimited text commands on a socket.
+  """
 
-	def __init__(self):
-		# Protect access
-		self._lock = threading.RLock()
-		self._running = True
+  def __init__(self):
+    # Protect access
+    self._lock = threading.RLock()
+    self._running = True
 
-	def __call__(self, socket):
-		"""Called for a connection."""
-		# Set timeout on socket operations
-		socket.settimeout(1)
+  def __call__(self, socket):
+    """Called for a connection."""
+    # Set timeout on socket operations
+    socket.settimeout(1)
 
-		# Wrap socket for events
-		wrappedSocket = Socket(socket)
-		
-		# Store the unprocessed data
-		stored = ''
-		chunk = ''
-		
-		# On connect!
-		self._lock.acquire()
-		self.onConnect(wrappedSocket)
-		self._lock.release()
-		
-		# Loop so long as the receiver is still running
-		while self.isRunning():
-		
-			# Take everything up to the first newline of the stored data
-			(message, sep, rest) = stored.partition('\n')
-			if sep == '': # If no newline is found, store more data...
-				while self.isRunning():
-					try:
-						chunk = ''
-						chunk = socket.recv(1024)
-						stored += chunk
-						break
-					except socketlib.timeout:
-						pass
-					except:
-						print 'EXCEPTION'
-				
-				# Empty chunk means disconnect
-				if chunk == '':
-					break;
+    # Wrap socket for events
+    wrappedSocket = Socket(socket)
+    
+    # Store the unprocessed data
+    stored = ''
+    chunk = ''
+    
+    # On connect!
+    self._lock.acquire()
+    self.onConnect(wrappedSocket)
+    self._lock.release()
+    
+    # Loop so long as the receiver is still running
+    while self.isRunning():
+    
+      # Take everything up to the first newline of the stored data
+      (message, sep, rest) = stored.partition('\n')
+      if sep == '': # If no newline is found, store more data...
+        while self.isRunning():
+          try:
+            chunk = ''
+            chunk = socket.recv(1024)
+            stored += chunk
+            break
+          except socketlib.timeout:
+            pass
+          except:
+            print 'EXCEPTION'
+        
+        # Empty chunk means disconnect
+        if chunk == '':
+          break;
 
-				continue
-			else: # ...otherwise store the rest
-				stored = rest			
-				
-			# Process the command
-			self._lock.acquire()
-			success = self.onMessage(wrappedSocket, message)
-			self._lock.release()
-			
-			if not success:
-				break;
+        continue
+      else: # ...otherwise store the rest
+        stored = rest     
+        
+      # Process the command
+      self._lock.acquire()
+      success = self.onMessage(wrappedSocket, message)
+      self._lock.release()
+      
+      if not success:
+        break;
 
-		# On disconnect!
-		self._lock.acquire()
-		self.onDisconnect(wrappedSocket)		
-		self._lock.release()
-		socket.close()
-		del socket
-		
-		# On join!
-		self.onJoin()
-			
-	def stop(self):
-		"""Stop this receiver."""
-		self._lock.acquire()
-		self._running = False
-		self._lock.release()
-		
-	def isRunning(self):
-		"""Is this receiver still running?"""
-		self._lock.acquire()
-		running = self._running
-		self._lock.release()
-		return running
-		
-	def onConnect(self, socket):
-		pass
+    # On disconnect!
+    self._lock.acquire()
+    self.onDisconnect(wrappedSocket)    
+    self._lock.release()
+    socket.close()
+    del socket
+    
+    # On join!
+    self.onJoin()
+      
+  def stop(self):
+    """Stop this receiver."""
+    self._lock.acquire()
+    self._running = False
+    self._lock.release()
+    
+  def isRunning(self):
+    """Is this receiver still running?"""
+    self._lock.acquire()
+    running = self._running
+    self._lock.release()
+    return running
+    
+  def onConnect(self, socket):
+    pass
 
-	def onMessage(self, socket, message):
-		pass
+  def onMessage(self, socket, message):
+    pass
 
-	def onDisconnect(self, socket):
-		pass
+  def onDisconnect(self, socket):
+    pass
 
-	def onJoin(self):
-		pass
+  def onJoin(self):
+    pass
 
-		
-		
+    
+    
 class Server(Receiver):
 
-	def start(self, ip, port):
-		# Set up server socket
-		serversocket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
-		serversocket.setsockopt(socketlib.SOL_SOCKET, socketlib.SO_REUSEADDR, 1)
-		serversocket.bind((ip, int(port)))
-		serversocket.listen(10)
-		serversocket.settimeout(1)
-		
-		# On start!
-		self.onStart()
+  def start(self, ip, port):
+    # Set up server socket
+    serversocket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
+    serversocket.setsockopt(socketlib.SOL_SOCKET, socketlib.SO_REUSEADDR, 1)
+    serversocket.bind((ip, int(port)))
+    serversocket.listen(10)
+    serversocket.settimeout(1)
+    
+    # On start!
+    self.onStart()
 
-		# Main connection loop
-		threads = []
-		while self.isRunning():
-			try:
-				(socket, address) = serversocket.accept()
-				thread = threading.Thread(target = self, args = (socket,))
-				threads.append(thread)
-				thread.start()
-			except socketlib.timeout:
-				pass
-			except:
-				self.stop()
+    # Main connection loop
+    threads = []
+    while self.isRunning():
+      try:
+        (socket, address) = serversocket.accept()
+        thread = threading.Thread(target = self, args = (socket,))
+        threads.append(thread)
+        thread.start()
+      except socketlib.timeout:
+        pass
+      except:
+        self.stop()
 
-		# Wait for all threads
-		while len(threads):
-			threads.pop().join()
+    # Wait for all threads
+    while len(threads):
+      threads.pop().join()
 
-		# On stop!				
-		self.onStop()
+    # On stop!        
+    self.onStop()
 
-	def onStart(self):
-		pass
+  def onStart(self):
+    pass
 
-	def onStop(self):
-		pass
+  def onStop(self):
+    pass
 
 class Client(Receiver):
-	
-	def start(self, ip, port):
-		# Set up server socket
-		self._socket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
-		self._socket.settimeout(1)
-		self._socket.connect((ip, int(port)))
+  
+  def start(self, ip, port):
+    # Set up server socket
+    self._socket = socketlib.socket(socketlib.AF_INET, socketlib.SOCK_STREAM)
+    self._socket.settimeout(1)
+    self._socket.connect((ip, int(port)))
 
-		# On start!
-		self.onStart()
+    # On start!
+    self.onStart()
 
-		# Start listening for incoming messages
-		self._thread = threading.Thread(target = self, args = (self._socket,))
-		self._thread.start()
-		
-	def send(self, message):
-		# Send message to server
-		self._lock.acquire()
-		self._socket.send("%s\n" % message.strip())
-		self._lock.release()
-		time.sleep(0.5)
+    # Start listening for incoming messages
+    self._thread = threading.Thread(target = self, args = (self._socket,))
+    self._thread.start()
+    
+  def send(self, message):
+    # Send message to server
+    self._lock.acquire()
+    self._socket.send("%s\n" % message.strip())
+    self._lock.release()
+    time.sleep(0.5)
 
-	def stop(self):
-		# Stop event loop
-		Receiver.stop(self)
-		
-		# Join thread
-		if self._thread != threading.currentThread():
-			self._thread.join()
-		
-		# On stop!
-		self.onStop()		
+  def stop(self):
+    # Stop event loop
+    Receiver.stop(self)
+    print("Stopping")
+    # Join thread
+    if self._thread != threading.currentThread():
+      self._thread.join()
+    
+    # On stop!
+    self.onStop()   
 
-	def onStart(self):
-		pass
+  def onStart(self):
+    pass
 
-	def onStop(self):
-		pass
-		
-	def onJoin(self):
-		self.stop()
+  def onStop(self):
+    pass
+    
+  def onJoin(self):
+    self.stop()
 
 
 # Added by Tom Richardson - 02/07/2013
@@ -273,8 +273,7 @@ class PiMoteServer(Server):
     if self.isPassword: #if the server has password, request it
       socket.send(str(PiMoteServer.REQUEST_PASSWORD))
     else: #otherwise setup
-      self.phone.setup(socket)
-    self.clientConnected(socket)
+      self.clientConnected(socket)
     return True
 
   #Called when a phone disconnects from the server
@@ -293,9 +292,9 @@ class PiMoteServer(Server):
   def managePassword(self, password, socket):
     if password == self.password: #Password was right, tell them to store key
       socket.send(str(PiMoteServer.STORE_KEY)+","+self.key)
-      self.phone.setup(socket)#setup
+      self.clientConnected(socket)
     elif password == self.key:#they had a key
-      self.phone.setup(socket)#setup
+      self.clientConnected(socket)
     else:#wrong password
       socket.send(str(PiMoteServer.PASSWORD_FAIL)) #kick them
 
