f0084b9ee627e82321f50cb9218421e3b5f14735
Tom Richardson <tom@ajrmail.co.uk>
2013-07-19 14:01:24 +0100

    Control all PiFace LED's with PiMote

--- PiMote/examples/pifaceexample.py
+++ PiMote/examples/pifaceexample.py
@@ -4,33 +4,38 @@ from pimote import *
 p.init()
 pfd = p.PiFaceDigital()
 
-ip = "0.0.0.0"
-port = 8090
-
 class MyPhone(Phone):
 	#Override
 	def buttonPressed(self, id, message, phoneId):
 		global pfd
-		#########----------------------------------------------###########
-		# Your code will go here! Check for the ID of the button pressed #
-		# and handle that button press as you wish.                      #
-		#########----------------------------------------------###########
-		if id == b.getId():
-			if b.getValue():
-				pfd.leds[0].turn_on()
-			else:
-				pfd.leds[0].turn_off()
+		global buttons
+		
+		j = 0
+		for j in range(0, 8):
+			if buttons[j].getId() == id:
+				self.changeLed(pfd, j, buttons[j])
+
+	def changeLed(self, pfd, led, b):
+		if b.getValue():
+			pfd.leds[led].turn_on()
+		else:
+			pfd.leds[led].turn_off()
 
 # Create the phone object
 thisphone = MyPhone()
 thisphone.setTitle("PiFace Control")
 
-b = ToggleButton("Toggle LED", False)
-thisphone.addButton(b)
+buttons = []
+
+i=0
+for i in range(0, 8):
+	b = ToggleButton("Toggle LED " + str(i), False)
+	buttons.append(b)
+	thisphone.addButton(b)
 
 #Create the server
 myserver = PhoneServer()
 #Add the phone
 myserver.addPhone(thisphone)
 # Start server
-myserver.start(ip, port)
+myserver.start("0.0.0.0", 8090)
