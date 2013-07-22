538a80472514edab41b05be8ef30c414eb8242bf
dimanui <abelgad2@cs.manchester.ac.uk>
2013-07-22 14:37:55 +0100

    Unncesary files

--- projects/image_processing_with_picam/basicTake.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from cv2 import *
-import os
-import time
-
-while True:
-	# grab an image from the camera
-    os.system("raspistill -w 400 -h 300 -t 0 -n -o cam_image.jpg")    
-   
