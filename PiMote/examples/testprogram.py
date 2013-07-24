8a2d68e4ee0b14d14106604c5b18121671c6fb01
MrTomRichy <pi@raspberrypi.(none)>
2013-07-24 14:52:22 +0100

    Full test program

--- PiMote/examples/testprogram.py
+++ PiMote/examples/testprogram.py
@@ -25,17 +25,17 @@ class MyPhone(Phone):
 		#########----------------------------------------------###########
 		if id == b1.getId():
 			self.buttonPress+=1
-			o1.setText("<font color=red><b>Regular Button</b></font>: you have pressed it " + str(self.buttonPress) + " times")
+			o1.setText("<font color=#CC0000><b>Regular Button</b></font>: you have pressed it " + str(self.buttonPress) + " times")
 		elif id == b2.getId():
-			o2.setText("<font color=red><b>Toggle Button</b></font>: " + message)
+			o2.setText("<font color=#CC0000><b>Toggle Button</b></font>: " + message)
 		elif id == b3.getId():
-			o3.setText("<font color=red><b>Input Text</b></font>: '" + message + "'")
+			o3.setText("<font color=#CC0000><b>Input Text</b></font>: '" + message + "'")
 		elif id == r.getId():
 			i = random.randint(0, 100)
 			p.setProgress(i)
-			o4.setText("<font color=red><b>Progress Bar</b></font>: <b>"+str(i)+"</b>%")
+			o4.setText("<font color=#CC0000><b>Progress Bar</b></font>: <b>"+str(i)+"</b>%")
 		elif id == vi.getId():
-			o5.setText("<font color=red><b>Voice Input</b></font>: '" + message + "'")
+			o5.setText("<font color=#CC0000><b>Voice Input</b></font>: '" + message + "'")
 
 # Create the phone object
 thisphone = MyPhone()
@@ -44,17 +44,18 @@ p = ProgressBar(100)
 b1 = Button("Hello") #Regular button
 b2 = ToggleButton("This is a toggle button", True) #Toggle
 b3 = InputText("Input text here") #Text Input
-title = OutputText("<div align='center'><font color=blue font-size=25px><b>Test Application</b></font></div>")
+title = OutputText("<div align='center'><font color=#35B5E5 font-size=25px><b>Test Application</b></font></div>")
 title.setTextSize(20)
 o = OutputText("This is a <i>test</i> application, controlling an RPi from and Android phone!"
 			   + "<br>It's really simple! And using the <b>HTML</b> markup, you can make it look really classy!")
-o1 = OutputText("<font color=red><b>Regular Button</b></font>:")
-o2 = OutputText("<font color=red><b>Toggle Button</b></font>:") #Output field
-o3 = OutputText("<font color=red><b>Input Text</b></font>:")
-o4 = OutputText("P<font color=red><b>Progress Bar</b></font>:")
-o5 = OutputText("<font color=red><b>Voice Input</b></font>:")
+o1 = OutputText("<font color=#CC0000><b>Regular Button</b></font>:")
+o2 = OutputText("<font color=#CC0000><b>Toggle Button</b></font>:") #Output field
+o3 = OutputText("<font color=#CC0000><b>Input Text</b></font>:")
+o4 = OutputText("<font color=#CC0000><b>Progress Bar</b></font>:")
+o5 = OutputText("<font color=#CC0000><b>Voice Input</b></font>:")
+o6 = OutputText("This is refreshing due to a <font color=#CC0000><b>RecurringInfo</b></font> which 'polls' the phone. It's like pressing a button over and over!")
+o7 = OutputText("<font color=#CC0000>Video Feed</font>")
 v = VideoFeed(640, 480) #Live video feed
-v2 = VideoFeed(640, 480) #Live video feed
 vi = VoiceInput() #Voice input
 s = Spacer(100)
 r = RecurringInfo(2000)
@@ -75,9 +76,13 @@ thisphone.addSpace(s)
 thisphone.addOutput(o4)
 thisphone.addOutput(p)
 thisphone.addButton(r)
+thisphone.addOutput(o6)
 thisphone.addSpace(s)
 thisphone.addOutput(o5)
 thisphone.addButton(vi)
+#thisphone.addSpace(s)
+#thisphone.addOutput(o7)
+#thisphone.addVideoFeed(v)
 
 #Create the server
 myserver = PhoneServer()
