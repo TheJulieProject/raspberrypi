b5e44b7acdf6f3598ef055a229ddf5f73d23f560
Tom Richardson <tom@ajrmail.co.uk>
2013-08-01 15:02:26 +0100

    Scrollbar through phone layout

--- PiMote/GUI/gui.py
+++ PiMote/GUI/gui.py
@@ -14,6 +14,54 @@ default_head += "#########----------------------------------------------########
 
 notice = "This program is still a very early build. No validation has been added to the fields, so be careful with input!"
 
+
+class VerticalScrolledFrame(tk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+    
+    """
+    def __init__(self, parent, *args, **kw):
+        tk.Frame.__init__(self, parent, *args, **kw)            
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = tk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=tk.NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
+
+        return
+
 def nameExists(name):
 	for c in components:
 		if c[1] == name:
@@ -102,7 +150,7 @@ def refresh_layout():
 	except Exception, e:
 		print("Problem when destroying, " + str(e))
 
-	inner_frame = tk.Frame(master=layout_frame, height=height)
+	inner_frame = tk.Frame(master=layout_frame.interior, height=height)
 	
 	row = 0
 	for c in components:
@@ -348,11 +396,10 @@ add_spacer.grid(row=9, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
 space = tk.Label(master=main_frame, width=10).grid(row=0, column=1)
 
 layout_label = tk.Label(master=main_frame, text="Phone Layout", width=20).grid(row=0, column=2)
-layout_frame = tk.Frame(master=main_frame, relief="raised", borderwidth=2, padx=5, pady=5)
+layout_frame = VerticalScrolledFrame(main_frame, relief="raised", borderwidth=2, padx=5, pady=5)
 layout_frame.grid(row=1, column=2, rowspan=10, sticky=tk.N+tk.S+tk.E+tk.W)
-inner_frame = tk.Frame(master=layout_frame)
+inner_frame = tk.Frame(master=layout_frame.interior)
 inner_frame.pack(fill="both")
-
 space2 = tk.Label(master=main_frame, width=10, height=3).grid(row=0, column=3)
 
 properties_label = tk.Label(master=main_frame, text="Properties", width=35).grid(row=0, column=4)
